3f34e48959cf41e12eaaa21b4e3dfbf4a13c4a29
Anisha <anshumankumarlal@gmail.com>
2022-10-02 15:23:03 +0530

    a few image changes

---
+++ index.js
@@ -0,0 +1,21 @@
+let nextbtn = document.querySelector('.next')
+let prevbtn = document.querySelector('.prev')
+
+
+
+let i = 0;
+var slider = document.getElementsByClassName("slide"); 
+
+nextbtn.addEventListener("click",function () {
+    slider[i];
+})
+
+
+// prevButton.addEventListener("click",function () {
+//   i--
+//   if(i<0){
+//     i = 4;
+//   }
+//   document.getElementById("img").setAttributeNS(null,"src",list[i])
+  
+// })
